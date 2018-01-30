48bde44ee4fc53d9b8a279860fc8c81e1682538f
Jonathan <jparrales15@gmail.com>
2018-01-29 22:33:00 -0500

    modificado inicio de sesion

--- ProyectoBses/src/espol/edu/ec/gui/controller/InicioSesionController.java
+++ ProyectoBses/src/espol/edu/ec/gui/controller/InicioSesionController.java
@@ -6,9 +6,13 @@ package espol.edu.ec.gui.controller;
  * and open the template in the editor.
  */
 
+import espol.edu.ec.conexion.Conectar;
 import javafx.event.ActionEvent;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -20,8 +24,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javax.swing.JOptionPane;
 /**
  * FXML Controller class
  *
@@ -32,7 +38,7 @@ public class InicioSesionController implements Initializable {
     @FXML
     private TextField txtUser;
     @FXML
-    private TextField txtContra;
+    private PasswordField txtContra;
     @FXML
     private ChoiceBox<String> choEmpl;
     @FXML
@@ -44,27 +50,76 @@ public class InicioSesionController implements Initializable {
     public void ingresar(ActionEvent event) throws IOException {
         //ESTA PARTE SOLO ES PARA FINES DE PRUEBA AQUI DEBE HABER LA CONDICION DE SI ES ADMINISTRADOR O RECEPCIONISTA
         if("Recepcionista".equals(choEmpl.getValue())){
-            ((Node)(event.getSource())).getScene().getWindow().hide();
-            Parent root = FXMLLoader.load(getClass().getResource("/espol/edu/ec/gui/fxml/VentanaRecepcionista.fxml"));
-            Stage stage=new Stage(); 
-            Scene scene = new Scene(root);
-            stage.setScene(scene);
-            stage.setResizable(false);
-            stage.setTitle("BIENVENIDO");
-            stage.show();
-        }else if("Administrador".equals(choEmpl.getValue())){
-            ((Node)(event.getSource())).getScene().getWindow().hide();
-            Parent root = FXMLLoader.load(getClass().getResource("/espol/edu/ec/gui/fxml/VentanaAdministrador.fxml"));
-            Stage stage=new Stage(); 
-            Scene scene = new Scene(root);
-            stage.setScene(scene);
-            stage.setResizable(false);
-            stage.setTitle("BIENVENIDO");
-            stage.show();
-        }
+            try{
+            System.out.println("Entro al try");
+            Conectar com=new Conectar();
+            Connection con = null;
+            con=com.getConnection();
+            PreparedStatement ps;
+            ResultSet res;
+            ps=con.prepareStatement("select t.cedula from trabajador t, registro_admin reg where reg.id_trabajador=t.cedula and reg.cargo='Recepcionista';");
+            res=ps.executeQuery();
+            if(res.next())
+            {
+                if(txtUser.getText().equals(txtContra.getText()) && txtUser.getText().equals(res.getString("cedula")) && txtUser.getText().equals(res.getString("cedula")))
+                {
+                        ((Node)(event.getSource())).getScene().getWindow().hide();
+                        Parent root = FXMLLoader.load(getClass().getResource("/espol/edu/ec/gui/fxml/VentanaRecepcionista.fxml"));
+                        Stage stage=new Stage(); 
+                        Scene scene = new Scene(root);
+                        stage.setScene(scene);
+                        stage.setResizable(false);
+                        stage.setTitle("BIENVENIDO");
+                        stage.show();
+                    
+                }
+                else
+                    {
+                        JOptionPane.showMessageDialog(null, "Usuario o contraseña incorrectos");
+                    }
+                    com.Desconectar();
+            }
         
+         }catch(Exception e){
+            System.out.println("Error al conectar: "+e);
+            }
+        }else if("Administrador".equals(choEmpl.getValue())){
+            try{
+                System.out.println("Entro al try");
+                Conectar com=new Conectar();
+                Connection con = null;
+                con=com.getConnection();
+                PreparedStatement ps;
+                ResultSet res;
+                ps=con.prepareStatement("select t.cedula from trabajador t, registro_admin reg where reg.id_trabajador=t.cedula and reg.cargo='Administrador';");
+                res=ps.executeQuery();
+                if(res.next())
+                {
+                    if(txtUser.getText().equals(txtContra.getText()) && txtUser.getText().equals(res.getString("cedula")) && txtUser.getText().equals(res.getString("cedula")))
+                    {
+
+                            ((Node)(event.getSource())).getScene().getWindow().hide();
+                            Parent root = FXMLLoader.load(getClass().getResource("/espol/edu/ec/gui/fxml/VentanaAdministrador.fxml"));
+                            Stage stage=new Stage(); 
+                            Scene scene = new Scene(root);
+                            stage.setScene(scene);
+                            stage.setResizable(false);
+                            stage.setTitle("BIENVENIDO");
+                            stage.show();
+
+                    }
+                    else
+                    {
+                        JOptionPane.showMessageDialog(null, "Usuario o contraseña incorrectos");
+                    }
+                    com.Desconectar();
+                }
+
+            }catch(Exception e){
+                System.out.println("Error al conectar: "+e);
+            }
+        }
     }
-    
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
