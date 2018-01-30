52ee3cfb8d0e3ef786d53eb8661e6d2b89d7819b
AlexUbillaTorres <33100671+AlexUbillaTorres@users.noreply.github.com>
2018-01-29 20:03:14 -0500

    Add files via upload

---
+++ queries.sql
@@ -0,0 +1,29 @@
+create view consultar_cliente
+as 
+select *
+from cliente;
+
+create view consultar_trabajador
+as 
+select *
+from trabajador;
+
+create view consultar_pago
+as 
+select *
+from rol_pago;
+
+create view consultar_progreso
+as 
+select *
+from progreso;
+
+create view consultar_membresia
+as 
+select *
+from membresia;
+
+create view consultar_implemento
+as 
+select *
+from implemento;
