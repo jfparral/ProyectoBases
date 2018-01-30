447455be608c307e30458bc56b3bf0e1caf39619
AlexUbillaTorres <33100671+AlexUbillaTorres@users.noreply.github.com>
2018-01-29 21:16:45 -0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -27,3 +27,19 @@ create view consultar_implemento
 as 
 select *
 from implemento;
+
+DELIMITER ||	
+CREATE PROCEDURE INSERTAR_CLIENTE(IN cedula varchar(10), IN nombre varchar(10), IN apellido varchar(10), IN fecha_nac date, IN sexo varchar(9), IN telefono varchar(20), IN direccion varchar(50), IN correo varchar(30))
+BEGIN		
+INSERT INTO	cliente	
+VALUES(cedula, nombre, apellido, fecha_nac, sexo, telefono, direccion, correo);	
+END||	
+DELIMITER ;	
+
+DELIMITER ||	
+CREATE PROCEDURE ELIMINAR_CLIENTE(IN cedulacliente varchar(10))
+BEGIN		
+DELETE FROM	cliente	
+WHERE cedula=cedulacliente;	
+END||	
+DELIMITER ;
